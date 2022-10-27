499d68262d55a295c3b3d417caaf7e885184d30a
tarekanwer <tito.a7mad@gmail.com>
2022-10-27 04:51:23 +0200

    edits

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import images from "./api/images";
 const routes = express.Router();
-routes.get("/", (req, res) => {
+routes.get("/", (req: express.Request, res: express.Response) => {
   res.send("main api route");
 });
 
--- src/services/image.ts
+++ src/services/image.ts
@@ -1,7 +1,16 @@
 import sharp from "sharp";
 import existCheck from "./existCheck";
+import fs from "fs";
 const SaveImage = async (imgName: string, width: number, height: number) => {
   const name = imgName.replace(/\.[^/.]+$/, "");
+  if (!existCheck("./public/images/thumb")) {
+    fs.mkdir("./public/images/thumb", (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("Directory created successfully!");
+    });
+  }
   const outPath = `./public/images/thumb/${name}-${width}-${height}.jpg`;
   const path = `./public/images/${imgName}`;
   const result = existCheck(path);
